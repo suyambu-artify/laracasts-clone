8cc01aec000fabc43cefa3f5c258bb3c696e0a5d
System Administrator <root@owls-MacBook-Pro.local>
2018-07-19 13:48:00 +0100

    login modal with vue

---
+++ resources/assets/js/components/loginModal.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Login</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+
+                    <div class="form-group row">
+                        <div class="col-md-12">
+                            <input type="email" class="form-control" name="email"  required autofocus placeholder="moktar@gmail.com">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-12">
+                            <input type="text" class="form-control" name="password"  required autofocus placeholder="******">
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="form-group ">
+                    <div class="col-md-6 offset-md-4">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="remember" id="remember">
+
+                            <label class="form-check-label" for="remember">
+                                Remember Me
+                            </label>
+                        </div>
+                </div>
+
+
+                    <div class="col-md-6">
+                        <button type="button" class="btn btn-primary">
+                            Login
+                        </button>
+                    </div>
+
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,11 @@
-hello world
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+           
+        </div>
+    </div>
+</div>
+@endsection
