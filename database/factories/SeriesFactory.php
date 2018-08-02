7d145f76eff26ab509a49cabf58954dc10accb73
almokhtar bekkour <moktarb911@gmail.com>
2018-08-02 16:08:01 +0100

    adding factory to series

---
+++ database/factories/SeriesFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Serie::class, function (Faker $faker) {
+
+    $title = $faker->sentence(5);
+
+    return [
+        'title'=>$title,
+        'slug'=>str_slug($title),
+        'image'=>$faker->imageUrl(),
+        'description'=>$faker->paragraph(5)
+    ];
+});
